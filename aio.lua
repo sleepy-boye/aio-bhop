c5e562948612c73fab879bcc670f526e2925df51
sleepy-boye <79385123+sleepy-boye@users.noreply.github.com>
2021-02-21 19:40:09 -0500

    Update aio.lua

--- aio.lua
+++ aio.lua
@@ -1,1 +1,220 @@
-local a=game:GetService("\x52\x75\x6E\x53\x65\x72\x76\x69\x63\x65") local m=game:GetService("\x55\x73\x65\x72\x49\x6E\x70\x75\x74\x53\x65\x72\x76\x69\x63\x65") local b=game:GetService("\x50\x6C\x61\x79\x65\x72\x73").LocalPlayer local _=b.Name local d=game.Workspace.Characters:WaitForChild(_) local i=Instance.new("\x53\x63\x72\x65\x65\x6E\x47\x75\x69",b.PlayerGui) i.Enabled=false local _=Instance.new("\x46\x72\x61\x6D\x65",i) _.BackgroundTransparency=1 _.Position=UDim2.new(0.0075,0,0.62,0) _.Size=UDim2.new(0.2,0,0.18,0) local q=Instance.new("\x54\x65\x78\x74\x42\x6F\x78",_) q.Name="\x42\x6F\x78\x31" q.BackgroundTransparency=1 q.Size=UDim2.new(1,0,0.2,0) q.Position=UDim2.new(0,0,0,0) q.Text="" q.TextXAlignment="\x4C\x65\x66\x74" q.TextColor3=Color3.new(255,255,255) q.TextSize=24 q.Font=Enum.Font.SourceSansBold local s=Instance.new("\x54\x65\x78\x74\x42\x6F\x78",_) s.Name="\x42\x6F\x78\x32" s.BackgroundTransparency=1 s.Size=UDim2.new(1,0,0.2,0) s.Position=UDim2.new(0,0,0.2,0) s.Text="" s.TextXAlignment="\x4C\x65\x66\x74" s.TextColor3=Color3.new(255,255,255) s.TextSize=24 s.Font=Enum.Font.SourceSansBold local t=Instance.new("\x54\x65\x78\x74\x42\x6F\x78",_) t.Name="\x42\x6F\x78\x33" t.BackgroundTransparency=1 t.Size=UDim2.new(1,0,0.2,0) t.Position=UDim2.new(0,0,0.4,0) t.Text="" t.TextXAlignment="\x4C\x65\x66\x74" t.TextColor3=Color3.new(255,255,255) t.TextSize=24 t.Font=Enum.Font.SourceSansBold local u=Instance.new("\x54\x65\x78\x74\x42\x6F\x78",_) u.Name="\x42\x6F\x78\x34" u.BackgroundTransparency=1 u.Size=UDim2.new(1,0,0.2,0) u.Position=UDim2.new(0,0,0.6,0) u.Text="" u.TextXAlignment="\x4C\x65\x66\x74" u.TextColor3=Color3.new(255,255,255) u.TextSize=24 u.Font=Enum.Font.SourceSansBold local w=Instance.new("\x54\x65\x78\x74\x42\x6F\x78",_) w.Name="\x42\x6F\x78\x35" w.BackgroundTransparency=1 w.Size=UDim2.new(1,0,0.2,0) w.Position=UDim2.new(0,0,0.8,0) w.Text="" w.TextXAlignment="\x4C\x65\x66\x74" w.TextColor3=Color3.new(255,255,255) w.TextSize=24 w.Font=Enum.Font.SourceSansBold local f=nil local c=nil local h=nil local e=nil local k=false local l=false local g=false local b=-3.5 local n=0 local v=0 local x=0 local o=0 local p=0 local r=0 local j=game.Players.LocalPlayer.PlayerGui:GetDescendants() local function _()for _,_ in pairs(j)do if _.ClassName=="\x54\x65\x78\x74\x4C\x61\x62\x65\x6C"and string.find(_.Text,"\x75\x2F\x73")then return(_)end end end repeat j=game.Players.LocalPlayer.PlayerGui:GetDescendants() wait(1) speedgui=_()until speedgui local function _()for _,_ in pairs(j)do if _.ClassName=="\x54\x65\x78\x74\x4C\x61\x62\x65\x6C"and string.find(_.Text,"\x54\x69\x6D\x65\x3A\x20")then return(_)end end end repeat j=game.Players.LocalPlayer.PlayerGui:GetDescendants() wait(1) timegui=_()until timegui for _,_ in pairs(getgc(true))do if type(_)=="\x74\x61\x62\x6C\x65"then if rawget(_,"\x43\x61\x6C\x6C")and rawget(_,"\x41\x64\x64")and rawget(_,"\x49\x6E\x69\x74\x4C\x61\x73\x74")and not h then h=_["\x43\x61\x6C\x6C"] e=_["\x41\x64\x64"]end if rawget(_,"\x43\x61\x6C\x6C")and rawget(_,"\x41\x64\x64")and not rawget(_,"\x49\x6E\x69\x74\x4C\x61\x73\x74")and not f then f=_["\x43\x61\x6C\x6C"] c=_["\x41\x64\x64"]end if h and f then break end end end local function _(_,a,b)e(tostring(b),b) h("\x41\x64\x64\x43\x6C\x69\x65\x6E\x74\x43\x6F\x6D\x6D\x61\x6E\x64",_,a,tostring(b))end _("\x6A\x61\x68\x47\x75\x69",{"\x4E\x75\x6D\x62\x65\x72"},function(_)if _==1 then k=true i.Enabled=true elseif _==0 then k=false i.Enabled=false q.Text="" s.Text="" t.Text="" u.Text="" w.Text=""else end end) m.InputChanged:Connect(function(_)local _=_.Delta if m:IsKeyDown(Enum.KeyCode.Space)and _.X>0 then if n==0 then n=2 v=v+1 elseif n==1 then n=2 v=v+1 end if m:IsKeyDown(Enum.KeyCode.D)then o=o+1 p=p+1 r=o/p else p=p+1 r=o/p end elseif m:IsKeyDown(Enum.KeyCode.Space)and _.X<0 then if n==0 then n=1 v=v+1 elseif n==2 then n=1 v=v+1 end if m:IsKeyDown(Enum.KeyCode.A)then o=o+1 p=p+1 r=o/p else p=p+1 r=o/p end elseif m:IsKeyDown(Enum.KeyCode.Space)and _.X==0 then end end) m.InputBegan:Connect(function(_)if _.KeyCode==Enum.KeyCode.Space and l==false and k==true then l=true x=x+1 w.Text="\x4A\x3A\x20"..x.."\x7C\x20\x53\x79\x6E\x63\x3A\x20"..(math.floor((r*10^4)+0.5)/(10^2)).."\x25\x20\x7C\x20\x53\x70\x64\x3A\x20"..tonumber(speedgui.Text:sub(0,-4)).."\x20\x7C\x20\x53\x74\x72\x61\x66\x65\x73\x3A\x20"..v print("\x53\x74\x61\x72\x74\x69\x6E\x67\x20\x72\x75\x6E\x21")elseif _.KeyCode==Enum.KeyCode.Space and l==true and k==true then print("\x41\x6C\x72\x65\x61\x64\x79\x20\x72\x75\x6E\x6E\x69\x6E\x67\x21")end end) local function _()if tonumber(timegui.Text:sub(13,15))>0 or tonumber(timegui.Text:sub(10,11))>0 or tonumber(timegui.Text:sub(7,8))>0 then local _=Ray.new(d.Torso.CFrame.Position,Vector3.new(0,b,0)) local _=workspace:FindPartOnRayWithIgnoreList(_,{d}) if _ and g==false and l==true and m:IsKeyDown(Enum.KeyCode.Space)then print("\x68\x69") print(x) g=true if x==0 then x=x+2 q.Text="" s.Text="" t.Text="" u.Text=w.Text w.Text="\x4A\x3A\x20"..x.."\x7C\x20\x53\x79\x6E\x63\x3A\x20"..(math.floor((r*10^4)+0.5)/(10^2)).."\x25\x20\x7C\x20\x53\x70\x64\x3A\x20"..tonumber(speedgui.Text:sub(0,-4)).."\x20\x7C\x20\x53\x74\x72\x61\x66\x65\x73\x3A\x20"..v print("\x4A\x3A\x20"..x.."\x7C\x20\x53\x79\x6E\x63\x3A\x20"..(math.floor((r*10^4)+0.5)/(10^2)).."\x25\x20\x7C\x20\x53\x70\x64\x3A\x20"..tonumber(speedgui.Text:sub(0,-4)).."\x20\x7C\x20\x53\x74\x72\x61\x66\x65\x73\x3A\x20"..v)elseif x==1 then x=x+1 q.Text="" s.Text="" t.Text=u.Text u.Text=w.Text w.Text="\x4A\x3A\x20"..x.."\x7C\x20\x53\x79\x6E\x63\x3A\x20"..(math.floor((r*10^4)+0.5)/(10^2)).."\x25\x20\x7C\x20\x53\x70\x64\x3A\x20"..tonumber(speedgui.Text:sub(0,-4)).."\x20\x7C\x20\x53\x74\x72\x61\x66\x65\x73\x3A\x20"..v print("\x4A\x3A\x20"..x.."\x7C\x20\x53\x79\x6E\x63\x3A\x20"..(math.floor((r*10^4)+0.5)/(10^2)).."\x25\x20\x7C\x20\x53\x70\x64\x3A\x20"..tonumber(speedgui.Text:sub(0,-4)).."\x20\x7C\x20\x53\x74\x72\x61\x66\x65\x73\x3A\x20"..v)elseif x==2 then x=x+1 q.Text="" s.Text=t.Text t.Text=u.Text u.Text=w.Text w.Text="\x4A\x3A\x20"..x.."\x7C\x20\x53\x79\x6E\x63\x3A\x20"..(math.floor((r*10^4)+0.5)/(10^2)).."\x25\x20\x7C\x20\x53\x70\x64\x3A\x20"..tonumber(speedgui.Text:sub(0,-4)).."\x20\x7C\x20\x53\x74\x72\x61\x66\x65\x73\x3A\x20"..v print("\x4A\x3A\x20"..x.."\x7C\x20\x53\x79\x6E\x63\x3A\x20"..(math.floor((r*10^4)+0.5)/(10^2)).."\x25\x20\x7C\x20\x53\x70\x64\x3A\x20"..tonumber(speedgui.Text:sub(0,-4)).."\x20\x7C\x20\x53\x74\x72\x61\x66\x65\x73\x3A\x20"..v)elseif x==3 then x=x+1 q.Text=s.Text s.Text=t.Text t.Text=u.Text u.Text=w.Text w.Text="\x4A\x3A\x20"..x.."\x7C\x20\x53\x79\x6E\x63\x3A\x20"..(math.floor((r*10^4)+0.5)/(10^2)).."\x25\x20\x7C\x20\x53\x70\x64\x3A\x20"..tonumber(speedgui.Text:sub(0,-4)).."\x20\x7C\x20\x53\x74\x72\x61\x66\x65\x73\x3A\x20"..v print("\x4A\x3A\x20"..x.."\x7C\x20\x53\x79\x6E\x63\x3A\x20"..(math.floor((r*10^4)+0.5)/(10^2)).."\x25\x20\x7C\x20\x53\x70\x64\x3A\x20"..tonumber(speedgui.Text:sub(0,-4)).."\x20\x7C\x20\x53\x74\x72\x61\x66\x65\x73\x3A\x20"..v)elseif x>3 then x=x+1 q.Text=s.Text s.Text=t.Text t.Text=u.Text u.Text=w.Text w.Text="\x4A\x3A\x20"..x.."\x7C\x20\x53\x79\x6E\x63\x3A\x20"..(math.floor((r*10^4)+0.5)/(10^2)).."\x25\x20\x7C\x20\x53\x70\x64\x3A\x20"..tonumber(speedgui.Text:sub(0,-4)).."\x20\x7C\x20\x53\x74\x72\x61\x66\x65\x73\x3A\x20"..v print("\x4A\x3A\x20"..x.."\x7C\x20\x53\x79\x6E\x63\x3A\x20"..(math.floor((r*10^4)+0.5)/(10^2)).."\x25\x20\x7C\x20\x53\x70\x64\x3A\x20"..tonumber(speedgui.Text:sub(0,-4)).."\x20\x7C\x20\x53\x74\x72\x61\x66\x65\x73\x3A\x20"..v)end wait(0.15) g=false v=0 end l=true else l=false x=0 v=0 n=0 o=0 p=0 r=0 end end a.RenderStepped:Connect(function()if k==true then _()end end)
+---------------------------------------------------------------------
+
+--- { SERVICES, VARIABLES, GENERAL SETUP } ---
+
+---------------------------------------------------------------------
+
+local RS = game:GetService("RunService")
+local UIS = game:GetService("UserInputService")
+
+local lP = game:GetService("Players").LocalPlayer
+local pN = lP.Name
+local pC = game.Workspace.Characters:WaitForChild(pN)
+
+local nG = Instance.new("ScreenGui", lP.PlayerGui); nG.Enabled = false
+local gF = Instance.new("Frame", nG); gF.BackgroundTransparency = 1; gF.Position = UDim2.new(0.0075,0,0.62,0); gF.Size = UDim2.new(0.2,0,0.18,0)
+local gTB1 = Instance.new("TextBox", gF); gTB1.Name = "Box1"; gTB1.BackgroundTransparency = 1; gTB1.Size = UDim2.new(1,0,0.2,0); gTB1.Position = UDim2.new(0,0,0,0); gTB1.Text = ""; gTB1.TextXAlignment = "Left"; gTB1.TextColor3 = Color3.new(255,255,255); gTB1.TextSize = 24; gTB1.Font = Enum.Font.SourceSansBold
+local gTB2 = Instance.new("TextBox", gF); gTB2.Name = "Box2"; gTB2.BackgroundTransparency = 1; gTB2.Size = UDim2.new(1,0,0.2,0); gTB2.Position = UDim2.new(0,0,0.2,0); gTB2.Text = ""; gTB2.TextXAlignment = "Left"; gTB2.TextColor3 = Color3.new(255,255,255); gTB2.TextSize = 24; gTB2.Font = Enum.Font.SourceSansBold
+local gTB3 = Instance.new("TextBox", gF); gTB3.Name = "Box3"; gTB3.BackgroundTransparency = 1; gTB3.Size = UDim2.new(1,0,0.2,0); gTB3.Position = UDim2.new(0,0,0.4,0); gTB3.Text = ""; gTB3.TextXAlignment = "Left"; gTB3.TextColor3 = Color3.new(255,255,255); gTB3.TextSize = 24; gTB3.Font = Enum.Font.SourceSansBold
+local gTB4 = Instance.new("TextBox", gF); gTB4.Name = "Box4"; gTB4.BackgroundTransparency = 1; gTB4.Size = UDim2.new(1,0,0.2,0); gTB4.Position = UDim2.new(0,0,0.6,0); gTB4.Text = ""; gTB4.TextXAlignment = "Left"; gTB4.TextColor3 = Color3.new(255,255,255); gTB4.TextSize = 24; gTB4.Font = Enum.Font.SourceSansBold
+local gTB5 = Instance.new("TextBox", gF); gTB5.Name = "Box5"; gTB5.BackgroundTransparency = 1; gTB5.Size = UDim2.new(1,0,0.2,0); gTB5.Position = UDim2.new(0,0,0.8,0); gTB5.Text = ""; gTB5.TextXAlignment = "Left"; gTB5.TextColor3 = Color3.new(255,255,255); gTB5.TextSize = 24; gTB5.Font = Enum.Font.SourceSansBold
+
+local localcall = nil
+local localadd = nil
+local remotecall = nil
+local remoteadd = nil
+
+local pdC = false
+local iR = false
+local rJ = false
+
+local rH = -3.5
+local sV = 0
+local nS = 0
+local nJ = 0
+local gS = 0
+local tS = 0
+local sP = 0
+
+local allguis = game.Players.LocalPlayer.PlayerGui:GetDescendants()
+local function getspeedgui() for i,v in pairs(allguis) do if v.ClassName == "TextLabel" and string.find(v.Text,"u/s") then return(v) end end end repeat allguis = game.Players.LocalPlayer.PlayerGui:GetDescendants(); wait(1); speedgui = getspeedgui(); until speedgui
+local function gettimegui() for i,v in pairs(allguis) do if v.ClassName == "TextLabel" and string.find(v.Text,"Time: ") then return(v) end end end repeat allguis = game.Players.LocalPlayer.PlayerGui:GetDescendants() wait(1)	timegui = gettimegui() until timegui
+
+---------------------------------------------------------------------
+
+--- { CUSTOM COMMANDS } ---
+
+---------------------------------------------------------------------
+
+for i,v in pairs(getgc(true)) do
+	if type(v) == "table" then
+		if rawget(v,"Call") and rawget(v,"Add") and rawget(v,"InitLast") and not remotecall then
+			remotecall = v["Call"]
+			remoteadd = v["Add"]
+		end
+		if rawget(v,"Call") and rawget(v,"Add") and not rawget(v,"InitLast") and not localcall then
+			localcall = v["Call"]
+			localadd = v["Add"]
+		end
+		if remotecall and localcall then
+			break
+		end
+	end
+end
+
+local function addCommand(name,validValues,func)
+	remoteadd(tostring(func),func)
+	remotecall("AddClientCommand",name,validValues,tostring(func))
+end
+
+addCommand("jahGui", {"Number"}, function(rawr)
+	if rawr == 1 then
+		pdC = true
+		nG.Enabled = true
+	elseif rawr == 0 then
+		pdC = false
+		nG.Enabled = false
+		gTB1.Text = ""
+		gTB2.Text = ""
+		gTB3.Text = ""
+		gTB4.Text = ""
+		gTB5.Text = ""
+	else
+		-- nothing
+	end
+end)
+
+---------------------------------------------------------------------
+
+--- { CUSTOM FUNCTIONS and UIS } ---
+
+---------------------------------------------------------------------
+
+
+UIS.InputChanged:Connect(function(input)
+	local delta = input.Delta
+	if UIS:IsKeyDown(Enum.KeyCode.Space) and delta.X > 0 then
+		if sV == 0 then
+			sV = 2
+			nS = nS + 1
+		elseif sV == 1 then
+			sV = 2
+			nS = nS + 1
+		end
+		
+		if UIS:IsKeyDown(Enum.KeyCode.D) then
+			gS = gS + 1
+			tS = tS + 1
+			sP = gS/tS
+		else
+			tS = tS + 1
+			sP = gS/tS
+		end
+	elseif UIS:IsKeyDown(Enum.KeyCode.Space) and delta.X < 0 then
+		if sV == 0 then
+			sV = 1
+			nS = nS + 1
+		elseif sV == 2 then
+			sV = 1
+			nS = nS + 1
+		end
+		
+		if UIS:IsKeyDown(Enum.KeyCode.A) then
+			gS = gS + 1
+			tS = tS + 1
+			sP = gS/tS
+		else
+			tS = tS + 1
+			sP = gS/tS
+		end
+	elseif UIS:IsKeyDown(Enum.KeyCode.Space) and delta.X == 0 then
+		-- Nothing
+	end
+end)
+
+
+UIS.InputBegan:Connect(function(input)
+	if input.KeyCode == Enum.KeyCode.Space and iR == false and pdC == true then
+		iR = true
+		nJ = nJ + 1
+		gTB5.Text = "J: " .. nJ .. "| Sync: " .. (math.floor((sP * 10^4) + 0.5)/(10^2)) .. "% | Spd: " .. tonumber(speedgui.Text:sub(0,-4)) .. " | Strafes: " .. nS
+		print("Starting run!")
+	elseif input.KeyCode == Enum.KeyCode.Space and iR == true and pdC == true then
+		print("Already running!")
+	end	
+end)
+
+local function pogDog()
+	if tonumber(timegui.Text:sub(13,15)) > 0 or tonumber(timegui.Text:sub(10,11)) > 0 or tonumber(timegui.Text:sub(7,8)) > 0 then
+		local ray = Ray.new(pC.Torso.CFrame.Position, Vector3.new(0, rH, 0))
+		local hit = workspace:FindPartOnRayWithIgnoreList(ray, {pC})
+		
+		if hit and rJ == false and iR == true and UIS:IsKeyDown(Enum.KeyCode.Space) then
+			print("hi")
+			print(nJ)
+			rJ = true
+			
+			if nJ == 0 then
+				nJ = nJ + 2
+				gTB1.Text = ""
+				gTB2.Text = ""
+				gTB3.Text = ""
+				gTB4.Text = gTB5.Text
+				gTB5.Text = "J: " .. nJ .. "| Sync: " .. (math.floor((sP * 10^4) + 0.5)/(10^2)) .. "% | Spd: " .. tonumber(speedgui.Text:sub(0,-4)) .. " | Strafes: " .. nS
+				print("J: " .. nJ .. "| Sync: " .. (math.floor((sP * 10^4) + 0.5)/(10^2)) .. "% | Spd: " .. tonumber(speedgui.Text:sub(0,-4)) .. " | Strafes: " .. nS)
+			elseif nJ == 1 then
+				nJ = nJ + 1
+				gTB1.Text = ""
+				gTB2.Text = ""
+				gTB3.Text = gTB4.Text
+				gTB4.Text = gTB5.Text
+				gTB5.Text = "J: " .. nJ .. "| Sync: " .. (math.floor((sP * 10^4) + 0.5)/(10^2)) .. "% | Spd: " .. tonumber(speedgui.Text:sub(0,-4)) .. " | Strafes: " .. nS
+				print("J: " .. nJ .. "| Sync: " .. (math.floor((sP * 10^4) + 0.5)/(10^2)) .. "% | Spd: " .. tonumber(speedgui.Text:sub(0,-4)) .. " | Strafes: " .. nS)
+			elseif nJ == 2 then
+				nJ = nJ + 1
+				gTB1.Text = ""
+				gTB2.Text = gTB3.Text
+				gTB3.Text = gTB4.Text
+				gTB4.Text = gTB5.Text
+				gTB5.Text = "J: " .. nJ .. "| Sync: " .. (math.floor((sP * 10^4) + 0.5)/(10^2)) .. "% | Spd: " .. tonumber(speedgui.Text:sub(0,-4)) .. " | Strafes: " .. nS
+				print("J: " .. nJ .. "| Sync: " .. (math.floor((sP * 10^4) + 0.5)/(10^2)) .. "% | Spd: " .. tonumber(speedgui.Text:sub(0,-4)) .. " | Strafes: " .. nS)
+			elseif nJ == 3 then
+				nJ = nJ + 1
+				gTB1.Text = gTB2.Text
+				gTB2.Text = gTB3.Text
+				gTB3.Text = gTB4.Text
+				gTB4.Text = gTB5.Text
+				gTB5.Text = "J: " .. nJ .. "| Sync: " .. (math.floor((sP * 10^4) + 0.5)/(10^2)) .. "% | Spd: " .. tonumber(speedgui.Text:sub(0,-4)) .. " | Strafes: " .. nS
+				print("J: " .. nJ .. "| Sync: " .. (math.floor((sP * 10^4) + 0.5)/(10^2)) .. "% | Spd: " .. tonumber(speedgui.Text:sub(0,-4)) .. " | Strafes: " .. nS)
+			elseif nJ > 3 then
+				nJ = nJ + 1
+				gTB1.Text = gTB2.Text
+				gTB2.Text = gTB3.Text
+				gTB3.Text = gTB4.Text
+				gTB4.Text = gTB5.Text
+				gTB5.Text = "J: " .. nJ .. "| Sync: " .. (math.floor((sP * 10^4) + 0.5)/(10^2)) .. "% | Spd: " .. tonumber(speedgui.Text:sub(0,-4)) .. " | Strafes: " .. nS
+				print("J: " .. nJ .. "| Sync: " .. (math.floor((sP * 10^4) + 0.5)/(10^2)) .. "% | Spd: " .. tonumber(speedgui.Text:sub(0,-4)) .. " | Strafes: " .. nS)
+			end
+			
+			wait(0.15)
+			rJ = false
+			nS = 0
+		end
+		
+		iR = true
+	else
+		iR = false
+		nJ = 0
+		nS = 0
+		sV = 0
+		gS = 0
+		tS = 0
+		sP = 0
+	end
+end
+
+RS.RenderStepped:Connect(function()
+	if pdC == true then
+		pogDog()
+	end
+end)
