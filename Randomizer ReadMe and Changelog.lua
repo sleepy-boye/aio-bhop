4cc88ac60714d8445bf28f42b343ca5d75cf8039
sleepy-boye <79385123+sleepy-boye@users.noreply.github.com>
2021-06-07 15:37:32 -0400

    Create Randomizer ReadMe and Changelog.lua

---
+++ Randomizer ReadMe and Changelog.lua
@@ -0,0 +1,55 @@
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
+                     -------------------------------------------------- { Read Me } --------------------------------------------------                     
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+How to Execute:
+  In synapse paste:
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/sleepy-boye/aio-bhop/main/randomizer.lua",true))()
+
+Current Commands:
+  /setdefs -- Allows you to change the default fov and sensitivity the script uses when changing your styles.
+
+Current Styles:
+  -- { Functional - 20 :: Total - 30 } --
+  Styles: 
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+                    -------------------------------------------------- { AiO : Changelog } --------------------------------------------------                    
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Version 1.0 :: 10-May-21
+  Added Randomizer Function
+  Styles: A-Only, Auto, Backwards, Half-Sideways, Sideways, W-Only
+  
+Version 1.01 :: 12-May-21
+  Added Faste and Slow
+  Styles: A-Only, Auto, Backwards, Faste, Half-Sideways, Sideways, Slow, W-Only
+
+  
+Version 1.02 :: 20-May-21
+  Added Left Faste, Right Faste, Timescale 0.5, Timescale 1.33, D-Only, and FOV 60
+  Styles: A-Only, Auto, Backwards, D-Only, Faste, FOV 60, Half-Sideways, Left Faste, Right Faste, Sideways, Slow, Timescale 0.5, Timescale 1.33, W-Only
+  
+Version 1.1 :: 29-May-21
+  AIDAN HAS JOINED THE BATTLE
+  Massive Rework of Randomizer System (Thanks to Aidan)
+  Fixed Left Faste and Right Faste (Reducing Lag a Ton)
+  Styles: A-Only, Auto, Backwards, D-Only, Faste, FOV 60, Half-Sideways, Left Faste, Right Faste, Sideways, Slow, Timescale 0.5, Timescale 1.33, W-Only
+  
+Version 1.11 :: 29-May-21
+  Added Low Gravity
+  Styles: A-Only, Auto, Backwards, D-Only, Faste, FOV 60, Half-Sideways, Left Faste, Low Gravity, Right Faste, Sideways, Slow, Timescale 0.5, Timescale 1.33, W-Only
+  
+Version 1.12 :: 31-May-21
+  Added Gain with Time and Third Person
+  Styles: A-Only, Auto, Backwards, D-Only, Faste, FOV 60, Gain with Time, Half-Sideways, Left Faste, Low Gravity, Right Faste, Sideways, Slow, Third Person,
+  Timescale 0.5, Timescale 1.33, W-Only
+
+Version 1.13 :: 03-Jun-21
+  Added Slowfall and Velocity Cap
+  Styles: A-Only, Auto, Backwards, D-Only, Faste, FOV 60, Gain with Time, Half-Sideways, Left Faste, Low Gravity, Right Faste, Sideways, Slow, Slowfall,
+  Third Person, Timescale 0.5, Timescale 1.33, Velocity Cap, W-Only
+
+Version 1.14 :: 07-Jun-21
+  Added Bomberman
+  Styles: A-Only, Auto, Backwards, D-Only, Faste, FOV 60, Gain with Time, Half-Sideways, Left Faste, Low Gravity, Right Faste, Sideways, Slow, Slowfall,
+  Third Person, Timescale 0.5, Timescale 1.33, Velocity Cap, W-Only
