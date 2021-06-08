627184b000a942f9fe42a2bbc4287c7330369025
sleepy-boye <79385123+sleepy-boye@users.noreply.github.com>
2021-06-08 15:20:15 -0400

    Update Randomizer ReadMe and Changelog.lua

--- Randomizer ReadMe and Changelog.lua
+++ Randomizer ReadMe and Changelog.lua
@@ -10,13 +10,13 @@ Current Commands:
   /setdefs -- Allows you to change the default fov and sensitivity the script uses when changing your styles.
 
 Current Styles:
-  -- { Functional - 22 :: Total - 30 } --
+  -- { Functional - 24 :: Total - 30 } --
   Styles: 
-      A-Only, Auto, Backwards, D-Only, Faste, Foggy Nights, FOV 60, Gain with Time, Half-Sideways, Left Faste, Low Gravity, Quake Pro, Right Faste, Sideways,
-      Slow, Slowfall, Third Person, Timescale 0.5, Timescale 1.33, Turbo, Velocity Cap, W-Only
+      A-Only, Auto, Atom Eve, Backwards, Builderman, D-Only, Faste, Foggy Nights, FOV 60, Gain with Time, Half-Sideways, Left Faste, Low Gravity, Quake Pro,
+      Right Faste, Sideways, Slow, Slowfall, Third Person, Timescale 0.5, Timescale 1.33, Turbo, Velocity Cap, W-Only
 
   Non-Functional Styles:
-      Atom Eve, Bomber Man, Builderman, Double Jump, Drunk Mode, Flashlight, Invisible Blocks, Landing Light
+      Bomber Man, Double Jump, Drunk Mode, Flashlight, Invisible Blocks, Landing Light, Trowel
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
                     ----------------------------------- { Randomizer : Changelog } --------------------------------------------------                    
@@ -25,56 +25,73 @@ Current Styles:
 Version 1.0 :: 10-May-21
   Added Randomizer Function
   
-  Styles: A-Only, Auto, Backwards, Half-Sideways, Sideways, W-Only
+  Styles: 
+    A-Only, Auto, Backwards, Half-Sideways, Sideways, W-Only
   
 Version 1.01 :: 12-May-21
   Added Faste, Foggy Nights, and Slow
   
-  Styles: A-Only, Auto, Backwards, Faste, Foggy Nights, Half-Sideways, Sideways, Slow, W-Only
+  Styles: 
+    A-Only, Auto, Backwards, Faste, Foggy Nights, Half-Sideways, Sideways, Slow, W-Only
 
   
 Version 1.02 :: 20-May-21
   Added Left Faste, Right Faste, Timescale 0.5, Timescale 1.33, D-Only, and FOV 60
   
-  Styles: A-Only, Auto, Backwards, D-Only, Faste, Foggy Nights, FOV 60, Half-Sideways, Left Faste, Right Faste, Sideways, Slow, Timescale 0.5, 
-  Timescale 1.33, W-Only
+  Styles: 
+      A-Only, Auto, Backwards, D-Only, Faste, Foggy Nights, FOV 60, Half-Sideways, Left Faste, Right Faste, Sideways, Slow, Timescale 0.5, 
+      Timescale 1.33, W-Only
   
 Version 1.1 :: 29-May-21
   AIDAN HAS JOINED THE BATTLE
   Massive Rework of Randomizer System (Thanks to Aidan)
   Fixed Left Faste and Right Faste (Reducing Lag a Ton)
   
-  Styles: A-Only, Auto, Backwards, D-Only, Faste, Foggy Nights, FOV 60, Half-Sideways, Left Faste, Right Faste, Sideways, Slow, Timescale 0.5,
-  Timescale 1.33, W-Only
+  Styles: 
+      A-Only, Auto, Backwards, D-Only, Faste, Foggy Nights, FOV 60, Half-Sideways, Left Faste, Right Faste, Sideways, Slow, Timescale 0.5,
+      Timescale 1.33, W-Only
   
 Version 1.11 :: 29-May-21
   Added Low Gravity
   
-  Styles: A-Only, Auto, Backwards, D-Only, Faste, Foggy Nights, FOV 60, Half-Sideways, Left Faste, Low Gravity, Right Faste, Sideways, Slow,
-  Timescale 0.5, Timescale 1.33, W-Only
+  Styles: 
+      A-Only, Auto, Backwards, D-Only, Faste, Foggy Nights, FOV 60, Half-Sideways, Left Faste, Low Gravity, Right Faste, Sideways, Slow,
+      Timescale 0.5, Timescale 1.33, W-Only
   
 Version 1.12 :: 31-May-21
   Added Gain with Time and Third Person
   
-  Styles: A-Only, Auto, Backwards, D-Only, Faste, Foggy Nights, FOV 60, Gain with Time, Half-Sideways, Left Faste, Low Gravity, Right Faste, Sideways,
-  Slow, Third Person, Timescale 0.5, Timescale 1.33, W-Only
+  Styles: 
+      A-Only, Auto, Backwards, D-Only, Faste, Foggy Nights, FOV 60, Gain with Time, Half-Sideways, Left Faste, Low Gravity, Right Faste, Sideways,
+      Slow, Third Person, Timescale 0.5, Timescale 1.33, W-Only
 
 Version 1.2 :: 03-Jun-21
   -- {{ 20 STYLES REACHED } --
   Added Slowfall and Velocity Cap
   
-  Styles: A-Only, Auto, Backwards, D-Only, Faste, Foggy Nights, FOV 60, Gain with Time, Half-Sideways, Left Faste, Low Gravity, Right Faste, Sideways,
-  Slow, Slowfall, Third Person, Timescale 0.5, Timescale 1.33, Velocity Cap, W-Only
+  Styles: 
+      A-Only, Auto, Backwards, D-Only, Faste, Foggy Nights, FOV 60, Gain with Time, Half-Sideways, Left Faste, Low Gravity, Right Faste, Sideways,
+      Slow, Slowfall, Third Person, Timescale 0.5, Timescale 1.33, Velocity Cap, W-Only
 
 Version 1.21 :: 07-Jun-21
   Added Quake Pro
   
-  Styles: A-Only, Auto, Backwards, D-Only, Faste, Foggy Nights, FOV 60, Gain with Time, Half-Sideways, Left Faste, Low Gravity, Quake Pro, Right Faste, Sideways,
-  Slow, Slowfall, Third Person, Timescale 0.5, Timescale 1.33, Velocity Cap, W-Only
+  Styles: 
+      A-Only, Auto, Backwards, D-Only, Faste, Foggy Nights, FOV 60, Gain with Time, Half-Sideways, Left Faste, Low Gravity, Quake Pro, Right Faste, Sideways,
+      Slow, Slowfall, Third Person, Timescale 0.5, Timescale 1.33, Velocity Cap, W-Only
   
 Version 1.22 :: 07-Jun-21
   Added Turbo
   Reworked Map Grabbing and Lighting Changes
   
-  Styles: A-Only, Auto, Backwards, D-Only, Faste, Foggy Nights, FOV 60, Gain with Time, Half-Sideways, Left Faste, Low Gravity, Quake Pro, Right Faste, Sideways,
-  Slow, Slowfall, Third Person, Timescale 0.5, Timescale 1.33, Turbo, Velocity Cap, W-Only
+  Styles: 
+      A-Only, Auto, Backwards, D-Only, Faste, Foggy Nights, FOV 60, Gain with Time, Half-Sideways, Left Faste, Low Gravity, Quake Pro, Right Faste, Sideways,
+      Slow, Slowfall, Third Person, Timescale 0.5, Timescale 1.33, Turbo, Velocity Cap, W-Only
+
+Version 1.23 :: 08-Jun-21
+  Added Atom Eve and Builderman
+  Rework of Raycasting (Changed to Jump Function)
+    
+  Styles: 
+      A-Only, Auto, Atom Eve, Backwards, Builderman, D-Only, Faste, Foggy Nights, FOV 60, Gain with Time, Half-Sideways, Left Faste, Low Gravity, Quake Pro,
+      Right Faste, Sideways, Slow, Slowfall, Third Person, Timescale 0.5, Timescale 1.33, Turbo, Velocity Cap, W-Only
